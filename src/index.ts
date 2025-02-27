1c05bac8990384fd56cae755ac7ad46b6552fdb0
diliptadha <diliptadha.dev@gmail.com>
2025-02-27 08:14:53 +0530

    add logging

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.route";
 import mainTaskRoutes from "./routes/mainTask.route";
+import morgan from 'morgan'
 
 const app = express();
 const PORT = process.env.PORT;
@@ -11,6 +12,7 @@ const PORT = process.env.PORT;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -25,9 +27,26 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
+// path not found
 app.use((req, res, next) => {
   res.status(404).json({
     message: "404 not found path",
   });
 });
+
+// global error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error('Error occurred:', {
+    method: req.method,
+    path: req.path,
+    error: err
+  });
+
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err.message,
+    stack: process.env.NODE_ENV === 'development' ? err.stack : {}
+  });
+});
+
 
